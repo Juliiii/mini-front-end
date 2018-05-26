b9771a41ae1ad73bd276b7cd22a54e8cb80f9a36
juliiiizhu(朱理锋) <juliiiizhu@tencent.com>
2018-05-26 11:16:28 +0800

    页面框架搭好

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,42 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    // 搜索页面
+    {
+      path: '/search',
+      name: 'Search',
+      component: HelloWorld
+    },
+    // 我要合租页面
+    {
+      path: '/rent',
+      name: 'Rent',
+      component: HelloWorld
+    },
+    // 个人中心页面
+    {
+      path: '/personal',
+      name: 'Personal',
+      component: HelloWorld
+    },
+    // 认证页面
+    {
+      path: '/auth',
+      name: 'Auth',
+      component: HelloWorld
+    },
+    // 评论页面
+    {
+      path: '/comment',
+      name: 'Comment',
+      component: HelloWorld
+    },
+    // 发布租房页面
+    {
+      path: '/publish',
+      name: 'Publish',
+      component: HelloWorld
     }
   ]
 })
