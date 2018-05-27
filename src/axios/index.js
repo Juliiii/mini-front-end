c64d530fce56b8ad37e97c5d673aa2bdbc623657
juliiiizhu(朱理锋) <juliiiizhu@tencent.com>
2018-05-27 19:16:39 +0800

    加入axios作为ajax，ivew作为UI框架，echarts作为数据可视化

---
+++ src/axios/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+// axios 配置
+axios.defaults.timeout = 5000;
+axios.defaults.baseURL = '';
+
+// // http request 拦截器
+// axios.interceptors.request.use(
+//   config => {
+//     if (store.state.token) {
+//       config.headers.Authorization = `token ${store.state.token}`;
+//     }
+//     return config;
+//   },
+//   err => {
+//     return Promise.reject(err);
+//   });
+
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+
+      }
+    }
+    return Promise.reject(error.response.data);
+  });
+
+export default axios;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import axios from './axios';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
-Vue.config.productionTip = false
+Vue.use(iView);
+Vue.config.productionTip = false;
+Vue.prototype.axios = axios;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +17,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
