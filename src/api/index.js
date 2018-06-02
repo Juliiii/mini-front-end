4765729f1010f96f16f0a5f05b0abfcec3162b86
juliiiizhu(朱理锋) <juliiiizhu@tencent.com>
2018-06-02 11:02:23 +0800

    先push

---
+++ src/api/index.js
@@ -0,0 +1,100 @@
+import axios from '../axios';
+import qs from 'querystring';
+
+class Api {
+  constructor() {
+    this.baseUrl = '';
+  }
+
+  /**
+   * 搜索公司名，获取匹配公司位置列表
+   * 
+   * @param {any} company_name 
+   * @memberof Api
+   */
+  async getAddressList(company_name) {
+    const params = qs.querystring({
+      company_name
+    });
+
+    const url = `${this.baseUrl}/search-by-name?${params}`;
+
+    const res = await axios.get(url);
+
+    return res;
+  }
+
+  /**
+   * 点击公司位置，获取房源评论列表
+   * 
+   * @param {any} {
+   *     company_id,
+   *     offset,
+   *     limit
+   *   } 
+   * @memberof Api
+   */
+  async getComments({
+    company_id,
+    offset,
+    limit
+  }) {
+    const params = qs.querystring({
+      company_id,
+      offset,
+      limit
+    });
+
+    const url = `${this.baseUrl}/search-by-company-id?${params}`;
+
+    const res = await axios.get(url);
+
+    return res;
+  }
+
+  /**
+   * 评论公司周边房源：填写表单
+   * 
+   * @param {any} {
+   *     uid,
+   *     cid,
+   *     village,
+   *     how_go,
+   *     how_long,
+   *     is_clean,
+   *     has_food,
+   *     has_mall,
+   *     description
+   *   } 
+   * @memberof Api
+   */
+  async publishComments({
+    uid,
+    cid,
+    village,
+    how_go,
+    how_long,
+    is_clean,
+    has_food,
+    has_mall,
+    description
+  }) {
+    const url = `${this.baseUrl}/comment`;
+
+    const res = await axios.post(url, {
+      uid,
+      cid,
+      village,
+      how_go,
+      how_long,
+      is_clean,
+      has_food,
+      has_mall,
+      description
+    });
+
+    return res;
+  }
+}
+
+export default new Api();
